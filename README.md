812beaa0f31d45cf2b476d1934723f884aa44d4e
avandyck <arnaud.vandyck@staff.voo.be>
2016-02-17 11:15:48 +0100

    Add module -core-hazelcast-spring

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
 # osgi-caching-test
 Caching tests in OSGi.
 
-The __master__ is where the __no cache__ is defined.
+The __-core__ is where the __no cache__ is defined. The cache tests
+are in other __-core-*__ bundle
 
 There are two examples of the cache implementation:
 
-* [hazelcast-spring](https://github.com/avdyk/osgi-caching-test/tree/hazelcast-spring)
-* [javax.cache](https://github.com/avdyk/osgi-caching-test/tree/javax.cache)
+* core-hazelcast-spring
+* core-javax.cache
 
 # master
 
 Master contains the api, the service, the command and the feature. The
 command has no cache implemented. It's just a starting point for the
 example.
+
+
+# hazelcast-spring
+
+Possibile configuration of hazelcast with a cache called _avdyk_:
+
+```xml
+    <map name="avdyk">
+        <backup-count>0</backup-count>
+        <async-backup-count>0</async-backup-count>
+        <time-to-live-seconds>5</time-to-live-seconds>
+        <max-idle-seconds>0</max-idle-seconds>
+    </map>
+
+```
---
+++ com.github.avdyk.osgi.caching.test-core-hazelcast-spring/src/main/java/com/github/avdyk/osgi/caching/test/core/OSGiDeveloperService.java
@@ -0,0 +1,64 @@
+package com.github.avdyk.osgi.caching.test.core;
+
+import com.github.avdyk.osgi.caching.test.api.DeveloperService;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named("com.github.avdyk.osgi.caching.testService")
+public class OSGiDeveloperService implements DeveloperService {
+
+    private static Logger logger = LoggerFactory.getLogger(OSGiDeveloperService.class);
+    private final HazelcastCacheManager cacheManager;
+
+    @Inject
+    public OSGiDeveloperService(HazelcastCacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Override
+    public List<String> getDevelopers(String service) {
+        logger.info("get developer for {}", service);
+        final List<String> devs;
+        final Cache cache = cacheManager.getCache("avdyk");
+        if (cache != null) {
+            final Cache.ValueWrapper valueWrapper = cache.get(service);
+            if (valueWrapper != null) {
+                final Object o = valueWrapper.get();
+                if (o != null) {
+                    logger.info("Value is in the cache");
+                    devs = (List<String>) valueWrapper.get();
+                } else {
+                    logger.info("Value has been lost!");
+                    devs = getDevs(service);
+                    cache.put(service, devs);
+                }
+            } else {
+                logger.warn("No value in the cache");
+                devs = getDevs(service);
+                cache.put(service, devs);
+            }
+        } else {
+            logger.warn("Unable to get the cache!");
+            devs = getDevs(service);
+        }
+        return devs;
+    }
+
+    private List<String> getDevs(String service) {
+        logger.info("Creating dev list for {}", service);
+        final List<String> devs = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            devs.add("Dev " + (i + 1) + " service " + service);
+        }
+        return devs;
+    }
+
+
+}
